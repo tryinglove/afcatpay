a0cb3ab72566acb9c53be86bb1ca9fdd514db26a
tryinglove <707078062@qq.com>
2017-12-18 10:05:01 +0800

    添加时间工具类

---
+++ common/src/main/java/com.cn/common/util/DateUtil.java
@@ -0,0 +1,29 @@
+package com.cn.common.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by john on 2017/12/18.
+ * util for date
+ */
+public class DateUtil {
+    private final static String DEFAULT_TIME_FORMAT = "yyyyMMddHHmmss";
+    /**
+     * 获取格式化的服务器时间
+     * @param format
+     *          日期模板：yyyyMMddHHmmss,yyyyMMdd...etc
+     * @return String
+     */
+
+    public static String getNowTime(String format){
+        String timeFormat = (format == null)?DEFAULT_TIME_FORMAT:format;
+
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat(timeFormat);
+
+        return formatter.format(date);
+    }
+}
