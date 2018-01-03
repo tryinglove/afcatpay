b0ffa170f506796d483e567ef46d216216c933ea
tryinglove <707078062@qq.com>
2018-01-03 17:10:59 +0800

    修改服务脚本

--- pay-deploy/src/main/assembly/bin/start.bat
+++ pay-deploy/src/main/assembly/bin/start.bat
@@ -5,13 +5,7 @@ cd ..\lib
 for %%i in (*) do set LIB_JARS=!LIB_JARS!;..\lib\%%i
 cd ..\bin
 
-if ""%1"" == ""debug"" goto debug
-
 java -Xms128m -Xmx1024m -XX:MaxPermSize=64M  -classpath ..\conf;%LIB_JARS% com.afcat.server.dubbo.Main
-goto end
-
-:debug
-java -Xms128m -Xmx1024m -XX:MaxPermSize=64M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -classpath ..\conf;%LIB_JARS% com.afcat.server.dubbo.Main
 goto end
 
 :end
